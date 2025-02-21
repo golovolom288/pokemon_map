2642ad256febd9ddb9e1f3beb3056ad2422023eb
golovolom288 <142310490+golovolom288@users.noreply.github.com>
2025-02-21 20:45:22 +0600

    Разрешил полям оставаться пустыми

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -6,7 +6,7 @@ class Pokemon(models.Model):
     title_en = models.CharField(max_length=200, default="", blank=True, verbose_name="Название на английском:")
     title_jp = models.CharField(max_length=200, default="", blank=True, verbose_name="Название на японском:")
     description = models.TextField(verbose_name="Описание покемона:", default="")
-    img = models.ImageField(null=True, upload_to="pokemon_images", verbose_name="Картинка покемона:")
+    img = models.ImageField(null=True, blank=True, upload_to="pokemon_images", verbose_name="Картинка покемона:")
     previous_evolution = models.ForeignKey("self", on_delete=models.CASCADE, verbose_name="Предыдущая эволюция:", null=True, blank=True, related_name="next_evolutions")
 
     def __str__(self):
@@ -18,13 +18,13 @@ class PokemonEntity(models.Model):
                                 related_name="Entities")
     lat = models.FloatField(verbose_name="Широта:")
     lon = models.FloatField(verbose_name="Долгота:")
-    appeared_at = models.DateTimeField(null=True, verbose_name="Появится в:")
-    disappeared_at = models.DateTimeField(null=True, verbose_name="Пропадёт в:")
-    level = models.FloatField(null=True, verbose_name="Уровень покемона:")
-    health = models.FloatField(null=True, verbose_name="Здоровье покемона:")
-    strength = models.FloatField(null=True, verbose_name="Сила покемона:")
-    defence = models.FloatField(null=True, verbose_name="Защита покемона:")
-    stamina = models.FloatField(null=True, verbose_name="Выносливость покемона:")
+    appeared_at = models.DateTimeField(null=True, blank=True, verbose_name="Появится в:")
+    disappeared_at = models.DateTimeField(null=True, blank=True, verbose_name="Пропадёт в:")
+    level = models.FloatField(default=0, blank=True, verbose_name="Уровень покемона:")
+    health = models.FloatField(default=0, blank=True, verbose_name="Здоровье покемона:")
+    strength = models.FloatField(default=0, blank=True, verbose_name="Сила покемона:")
+    defence = models.FloatField(default=0, blank=True, verbose_name="Защита покемона:")
+    stamina = models.FloatField(default=0, blank=True, verbose_name="Выносливость покемона:")
 
     def __str__(self):
         return '{}'.format(self.pokemon)
